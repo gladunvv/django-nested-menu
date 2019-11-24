ffe099916e6e64d1c2763c89ee21f35309cb0f15
vladislav <bincha.1997@gmail.com>
2019-11-24 23:17:06 +0300

    Added menu models

--- nested_menu/menu/admin.py
+++ nested_menu/menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import Menu, Item
+
+
+@admin.register(Item)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent',]
+    list_filter = ['parent',]
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['title',]
--- nested_menu/menu/models.py
+++ nested_menu/menu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Menu title')
+    slug = models.SlugField(max_length=255, verbose_name="Menu slug")
+
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menus'
+
+    def __str__(self):
+        return self.title
+
+
+class Item(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='childrens', on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, blank=True, related_name='items', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name="Item slug")
+
+    class Meta:
+        verbose_name = 'Menu item'
+        verbose_name_plural = 'Menu items'
+
+    def __str__(self):
+        return self.title
